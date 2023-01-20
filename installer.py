6256ad0b0766175099e6c98cc555547a80fd83f0
SuperRedingBros <redingtommy@gmail.com>
2023-01-20 16:05:33 -0500

    test3

--- installer.py
+++ installer.py
@@ -8,7 +8,7 @@ if(not os.path.exists(file)):
     repo = git.Repo.clone_from('https://github.com/SuperRedingBros/RKitInstaller.git', file)
 else:
     repo = git.Repo(file)
-    #print(repo.remotes.origin.pull())
+    repo.remotes.origin.pull()
     repo.close()
 script = open(file+"/Main.py")
 exec(script.read())
